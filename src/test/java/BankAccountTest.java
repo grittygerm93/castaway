b90489eaf9f47b67ad31018efb6cefafbb497389
grittygerm <alexius.lzc@gmail.com>
2021-12-06 17:05:07 +0800

    added tests

---
+++ src/test/java/BankAccountTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankAccountTest {
+    BankAccount bankaccount;
+
+    @BeforeEach
+    void setUp() {
+        bankaccount = new BankAccount("Alex");
+    }
+
+    @Test
+    @DisplayName("test balance")
+    void getBalance() {
+        assertTrue(bankaccount.getBalance() == 0f );
+    }
+}
